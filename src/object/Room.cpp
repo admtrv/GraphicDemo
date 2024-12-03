c6e6a0af6715a252405f827e2077cfb557cab798
Anton Dmitriev <tosha.dmitriefff@yandex.ru>
2024-12-03 18:36:22 +0100

    Room: change from components to childs

--- src/object/Room.cpp
+++ src/object/Room.cpp
@@ -1,7 +1,3 @@
-/*
- * Room.cpp
- */
-
 #include "Room.h"
 
 Room::Room() {
@@ -9,33 +5,33 @@ Room::Room() {
     auto floor = std::make_unique<StaticObject>("floor.bmp");
     floor->position = glm::vec3(0.0f, 0.0f, 0.0f);
     floor->scale = glm::vec3(ROOM_WIDTH, ROOM_WALL_THICKNESS, ROOM_DEPTH);
-    components.push_back(std::move(floor));
+    addChild(std::move(floor));
 
     // ceiling
     auto ceiling = std::make_unique<StaticObject>("ceiling.bmp");
     ceiling->position = glm::vec3(0.0f, ROOM_HEIGHT, 0.0f);
     ceiling->scale = glm::vec3(ROOM_WIDTH, ROOM_WALL_THICKNESS, ROOM_DEPTH);
-    components.push_back(std::move(ceiling));
+    addChild(std::move(ceiling));
 
     // back wall
     auto backWall = std::make_unique<StaticObject>("wall.bmp");
     backWall->position = glm::vec3(0.0f, ROOM_HEIGHT / 2.0f, -ROOM_DEPTH / 2.0f);
     backWall->scale = glm::vec3(ROOM_WIDTH, ROOM_HEIGHT, ROOM_WALL_THICKNESS);
-    components.push_back(std::move(backWall));
+    addChild(std::move(backWall));
 
     // left wall
     auto leftWall = std::make_unique<StaticObject>("wall.bmp");
     leftWall->position = glm::vec3(-ROOM_WIDTH / 2.0f, ROOM_HEIGHT / 2.0f, 0.0f);
     leftWall->scale = glm::vec3(ROOM_DEPTH, ROOM_HEIGHT, ROOM_WALL_THICKNESS);
     leftWall->rotation = glm::vec3(0.0f, 0.0f, glm::radians(90.0f));
-    components.push_back(std::move(leftWall));
+    addChild(std::move(leftWall));
 
     // right wall
     auto rightWall = std::make_unique<StaticObject>("wall.bmp");
     rightWall->position = glm::vec3(ROOM_WIDTH / 2.0f, ROOM_HEIGHT / 2.0f, 0.0f);
     rightWall->scale = glm::vec3(ROOM_DEPTH, ROOM_HEIGHT, ROOM_WALL_THICKNESS);
     rightWall->rotation = glm::vec3(0.0f, 0.0f, glm::radians(90.0f));
-    components.push_back(std::move(rightWall));
+    addChild(std::move(rightWall));
 }
 
 void Room::addArcades() {
@@ -53,11 +49,11 @@ void Room::addDartboards() {
 
     auto leftDartboard = std::make_unique<Dartboard>();
     leftDartboard->position = glm::vec3(-offsetX, dartboardY, dartboardZ);
-    components.push_back(std::move(leftDartboard));
+    addChild(std::move(leftDartboard));
 
     auto rightDartboard = std::make_unique<Dartboard>();
     rightDartboard->position = glm::vec3(offsetX, dartboardY, dartboardZ);
-    components.push_back(std::move(rightDartboard));
+    addChild(std::move(rightDartboard));
 }
 
 // random arcade generation
@@ -81,7 +77,7 @@ void Room::generateArcade() {
 
         arcadeMachine->rotation = glm::vec3(0.0f, 0.0f, glm::radians(90.0f));
 
-        components.push_back(std::move(arcadeMachine));
+        addChild(std::move(arcadeMachine));
     }
 }
 
@@ -179,20 +175,6 @@ void Room::addChandeliers() {
                 startZ + i * stepZ
         );
 
-        components.push_back(std::move(chandelier));
-    }
-}
-
-
-bool Room::updateInternal(Scene& scene, float dt) {
-    for (auto& component : components) {
-        component->update(scene, dt);
-    }
-    return true;
-}
-
-void Room::renderInternal(Scene& scene) {
-    for (auto& component : components) {
-        component->render(scene);
+        addChild(std::move(chandelier));
     }
 }
--- src/object/Room.h
+++ src/object/Room.h
@@ -1,7 +1,3 @@
-/*
- * Room.cpp
- */
-
 #pragma once
 
 #include "src/object/StaticObject.h"
@@ -22,19 +18,16 @@
 #define ROOM_HEIGHT             10.0f
 #define ROOM_WALL_THICKNESS     0.2f
 
-
 class Room : public StaticObject {
 public:
     Room();
-    bool updateInternal(Scene& scene, float dt) override;
-    void renderInternal(Scene& scene) override;
 
     void addArcades();
     void addBilliards();
     void addDartboards();
     void addChandeliers();
+
 private:
-    std::vector<std::unique_ptr<StaticObject>> components;
     std::vector<std::unique_ptr<CollisionGroup>> collisionGroups;
 
     void generateArcade();
